e48555958f08686e9456d9af54fced6e20a69c37
Martin Camacho <martin@mcamac.com>
2014-08-18 19:46:19 -0400

    Add sass to gulpfile, add active class to sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
+var sass = require('gulp-ruby-sass');
 
 var paths = {
-  scripts: ['client/static/js/**/*.coffee', 'shared/**/*.coffee']
+  scripts: ['client/static/js/**/*.coffee', 'shared/**/*.coffee'],
+  styles: ['client/static/css/**/*.sass']
 };
 
 gulp.task('scripts', function () {
@@ -13,7 +15,13 @@ gulp.task('scripts', function () {
     .pipe(gulp.dest('client/static/js'))
 });
 
+gulp.task('sass', function () {
+  return gulp.src(paths.styles)
+    .pipe(sass())
+    .pipe(gulp.dest('client/static/css'));
+});
 
 gulp.task('watch', function () {
   gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.styles, ['sass']);
 });
