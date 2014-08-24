39ccf0c64824262c0497c58dfe641a1617ba94d5
Martin Camacho <martin@mcamac.com>
2014-08-23 21:49:39 -0400

    Add gulp-plumber to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var sass = require('gulp-ruby-sass');
+var plumber = require('gulp-plumber');
 
 var paths = {
   scripts: ['shared/**/*.coffee', 'client/static/js/**/*.coffee'],
@@ -10,6 +11,7 @@ var paths = {
 
 gulp.task('scripts', function () {
   return gulp.src(paths.scripts)
+    .pipe(plumber())
     .pipe(concat('all.coffee'))
     .pipe(coffee())
     .pipe(gulp.dest('client/static/js'));
@@ -17,6 +19,7 @@ gulp.task('scripts', function () {
 
 gulp.task('sass', function () {
   return gulp.src(paths.styles)
+    .pipe(plumber())
     .pipe(sass())
     .pipe(gulp.dest('client/static/css'));
 });
